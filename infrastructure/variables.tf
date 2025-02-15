b5312a17ab1cfab775dcd51a663c9fd767157ca5
Vincent Caggiari <vincent@caggiari.com>
2025-02-15 21:15:31 +0100

    rework

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,7 +3,17 @@ variable "github_owner" {
   type        = string
 }
 
-variable "github_token" {
-  type      = string
-  sensitive = true
+variable "github_app_id" {
+  description = "GitHub App ID"
+  type        = number
+}
+
+variable "github_app_installation_id" {
+  description = "GitHub App Installation ID"
+  type        = number
+}
+
+variable "github_app_private_key_path" {
+  description = "Path to the GitHub App private key"
+  type        = string
 }
