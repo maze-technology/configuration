e27e453a5cfd319c368d1d6b5955f8da9c8c8df4
Vincent Caggiari <vincent@caggiari.com>
2025-03-02 21:17:57 +0100

    typo debug

--- infrastructure/repositories.tf
+++ infrastructure/repositories.tf
@@ -30,7 +30,7 @@ resource "github_repository" "repo" {
 }
 
 resource "github_repository_file" "license" {
-  for_each       = { for repo in local.local.computed_repositories : repo.name => repo }
+  for_each       = { for repo in local.computed_repositories : repo.name => repo }
   repository     = github_repository.repo[each.key].name
   file           = "LICENSE"
   content        = file("${path.module}/repositories-LICENSE")
