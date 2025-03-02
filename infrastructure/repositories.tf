7f0921878e351e0437cfaf30da722f92423bad32
Vincent Caggiari <vincent@caggiari.com>
2025-03-02 15:35:25 +0100

    docs repository

--- infrastructure/repositories.tf
+++ infrastructure/repositories.tf
@@ -17,6 +17,25 @@ resource "github_repository" "github" {
   delete_branch_on_merge  = true
   auto_init               = false
   vulnerability_alerts    = true
+}
+
+resource "github_repository" "docs" {
+  name                    = "docs"
+  description             = "Documentation repository"
+  visibility              = "public"
+  has_issues              = true
+  has_wiki                = false
+  has_projects            = false
+  has_downloads           = false
+  archive_on_destroy      = true
+  is_template             = false
+  allow_merge_commit      = false
+  allow_squash_merge      = true
+  allow_rebase_merge      = false
+  allow_auto_merge        = false
+  delete_branch_on_merge  = true
+  auto_init               = false
+  vulnerability_alerts    = true
 
   pages {
     source {
