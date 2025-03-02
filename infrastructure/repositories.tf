60af533854b39f852523ea95b5715bf3cf1a7ee4
Vincent Caggiari <vincent@caggiari.com>
2025-03-02 16:27:54 +0100

    refac + default license for repositories

--- infrastructure/repositories.tf
+++ infrastructure/repositories.tf
@@ -1,15 +1,14 @@
-# Define repositories
-
-resource "github_repository" "github" {
-  name                    = ".github"
-  description             = "Github repository"
-  visibility              = "public"
+resource "github_repository" "repo" {
+  for_each                = { for repo in var.repositories : repo.name => repo }
+  name                    = each.value.name
+  description             = each.value.description
+  visibility              = each.value.visibility
   has_issues              = true
   has_wiki                = false
   has_projects            = false
   has_downloads           = false
   archive_on_destroy      = true
-  is_template             = false
+  is_template             = each.value.is_template
   allow_merge_commit      = false
   allow_squash_merge      = true
   allow_rebase_merge      = false
@@ -17,68 +16,24 @@ resource "github_repository" "github" {
   delete_branch_on_merge  = true
   auto_init               = false
   vulnerability_alerts    = true
-}
 
-resource "github_repository" "docs" {
-  name                    = "docs"
-  description             = "Documentation repository"
-  visibility              = "public"
-  has_issues              = true
-  has_wiki                = false
-  has_projects            = false
-  has_downloads           = false
-  archive_on_destroy      = true
-  is_template             = false
-  allow_merge_commit      = false
-  allow_squash_merge      = true
-  allow_rebase_merge      = false
-  allow_auto_merge        = false
-  delete_branch_on_merge  = true
-  auto_init               = true
-  vulnerability_alerts    = true
+  dynamic "pages" {
+    for_each = each.value.dynamic_pages ? [1] : []
 
-  pages {
-    source {
-      branch = "main"
-      path   = "/"
+    content {
+      source {
+        branch = "main"
+        path   = "/"
+      }
     }
   }
 }
 
-resource "github_repository" "commons" {
-  name                    = "commons"
-  description             = "Commons library repository"
-  visibility              = "public"
-  has_issues              = true
-  has_wiki                = false
-  has_projects            = false
-  has_downloads           = false
-  archive_on_destroy      = true
-  is_template             = false
-  allow_merge_commit      = false
-  allow_squash_merge      = true
-  allow_rebase_merge      = false
-  allow_auto_merge        = false
-  delete_branch_on_merge  = true
-  auto_init               = false
-  vulnerability_alerts    = true
-}
-
-resource "github_repository" "java-service-template" {
-  name                    = "java-service-template"
-  description             = "Java service base template repository"
-  visibility              = "public"
-  has_issues              = true
-  has_wiki                = false
-  has_projects            = false
-  has_downloads           = false
-  archive_on_destroy      = true
-  is_template             = true
-  allow_merge_commit      = false
-  allow_squash_merge      = true
-  allow_rebase_merge      = false
-  allow_auto_merge        = false
-  delete_branch_on_merge  = true
-  auto_init               = false
-  vulnerability_alerts    = true
+resource "github_repository_file" "license" {
+  for_each       = { for repo in var.repositories : repo.name => repo }
+  repository     = each.value.name
+  file           = "LICENSE"
+  content        = file("${path.module}/repositories-LICENSE")
+  branch         = "main"
+  commit_message = "Add LICENSE file"
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -17,3 +17,43 @@ variable "github_app_private_key_path" {
   description = "Path to the GitHub App private key"
   type        = string
 }
+
+variable "repositories" {
+  type = list(object({
+    name          = string
+    description   = string
+    visibility    = string
+    is_template   = bool
+    dynamic_pages = bool
+  }))
+  default = [
+    {
+      name          = ".github"
+      description   = "Github repository"
+      visibility    = "public"
+      is_template   = false
+      dynamic_pages = false
+    },
+    {
+      name          = "docs"
+      description   = "Documentation repository"
+      visibility    = "public"
+      is_template   = false
+      dynamic_pages = true
+    },
+    {
+      name          = "commons"
+      description   = "Commons library repository"
+      visibility    = "public"
+      is_template   = false
+      dynamic_pages = false
+    },
+    {
+      name          = "java-service-template"
+      description   = "Java service base template repository"
+      visibility    = "public"
+      is_template   = true
+      dynamic_pages = false
+    }
+  ]
+}
