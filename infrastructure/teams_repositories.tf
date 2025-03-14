742bf835d739401ef09d1b24f31e59fd5946eebb
Vincent Caggiari <vincent@caggiari.com>
2025-03-14 23:05:06 +0100

    moved file

--- infrastructure/teams_repositories.tf
+++ infrastructure/teams_repositories.tf
@@ -2,7 +2,7 @@ resource "github_team_repository" "push_teams" {
   for_each = {
     for pair in local.repo_push_team_pairs :
     "${pair.repo_name}-${pair.team_name}" => pair
-    }
+  }
 
   team_id    = github_team.teams[each.value.team_name].id
   repository = github_repository.repo[each.value.repo_name].name
