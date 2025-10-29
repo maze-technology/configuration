2baa177b6d8bce11304daeb6fb9b3047b6059378
Vincent Caggiari <vincent@caggiari.com>
2025-10-29 23:42:46 +0100

    debug teams cycle

--- infrastructure/github_teams.tf
+++ infrastructure/github_teams.tf
@@ -11,20 +11,27 @@ locals {
     "infrastructure-engineers" = {
       description    = "Maze Infrastructure engineers"
       privacy        = "closed"
-      parent_team_id = github_team.teams["engineers"].id
+      parent_team    = "engineers"
     },
     "release-engineers" = {
       description    = "Maze Release engineers"
       privacy        = "closed"
-      parent_team_id = github_team.teams["engineers"].id
+      parent_team    = "engineers"
     },
   }
 }
 
-resource "github_team" "teams" {
-  for_each       = local.teams_config
+resource "github_team" "parents" {
+  for_each    = { for k, v in local.teams_config : k => v if !contains(keys(v), "parent_team") }
+  name        = each.key
+  description = each.value.description
+  privacy     = each.value.privacy
+}
+
+resource "github_team" "children" {
+  for_each       = { for k, v in local.teams_config : k => v if contains(keys(v), "parent_team") }
   name           = each.key
   description    = each.value.description
   privacy        = each.value.privacy
-  parent_team_id = each.value.parent_team_id
+  parent_team_id = github_team.parents[each.value.parent_team].id
 }
