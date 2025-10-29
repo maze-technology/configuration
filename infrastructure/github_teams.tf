ea71de851ef03239fda8f36280da9af723b9b4bb
Vincent Caggiari <vincent@caggiari.com>
2025-10-29 23:36:59 +0100

    format

--- infrastructure/github_teams.tf
+++ infrastructure/github_teams.tf
@@ -9,22 +9,22 @@ locals {
       privacy     = "closed"
     },
     "infrastructure-engineers" = {
-      description = "Maze Infrastructure engineers"
-      privacy     = "closed"
+      description    = "Maze Infrastructure engineers"
+      privacy        = "closed"
       parent_team_id = github_team.teams["engineers"].id
     },
     "release-engineers" = {
-      description = "Maze Release engineers"
-      privacy     = "closed"
+      description    = "Maze Release engineers"
+      privacy        = "closed"
       parent_team_id = github_team.teams["engineers"].id
     },
   }
 }
 
 resource "github_team" "teams" {
-  for_each    = local.teams_config
-  name        = each.key
-  description = each.value.description
-  privacy     = each.value.privacy
+  for_each       = local.teams_config
+  name           = each.key
+  description    = each.value.description
+  privacy        = each.value.privacy
   parent_team_id = each.value.parent_team_id
 }
