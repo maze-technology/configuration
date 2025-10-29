8950d2e590ee8cc0ea1625b5892bdcae3dff30cb
Vincent Caggiari <vincent@caggiari.com>
2025-10-29 23:34:22 +0100

    rework codeowners, renovate and github teams

--- infrastructure/github_teams.tf
+++ infrastructure/github_teams.tf
@@ -4,30 +4,20 @@ locals {
       description = "Maze Project managers"
       privacy     = "closed"
     },
-    "release-engineers" = {
-      description = "Maze Release engineers"
-      privacy     = "closed"
-    },
-    "github-engineers" = {
-      description = "Maze GitHub engineers"
-      privacy     = "closed"
-    },
-    "spec-engineers" = {
-      description = "Maze Specification engineers"
+    "engineers" = {
+      description = "Maze Engineers"
       privacy     = "closed"
     },
     "infrastructure-engineers" = {
       description = "Maze Infrastructure engineers"
       privacy     = "closed"
+      parent_team_id = github_team.teams["engineers"].id
     },
-    "java-engineers" = {
-      description = "Maze Java engineers"
+    "release-engineers" = {
+      description = "Maze Release engineers"
       privacy     = "closed"
+      parent_team_id = github_team.teams["engineers"].id
     },
-    "python-engineers" = {
-      description = "Maze Python engineers"
-      privacy     = "closed"
-    }
   }
 }
 
@@ -36,4 +26,5 @@ resource "github_team" "teams" {
   name        = each.key
   description = each.value.description
   privacy     = each.value.privacy
+  parent_team_id = each.value.parent_team_id
 }
